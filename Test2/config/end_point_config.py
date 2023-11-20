6c5e5c911db104de9b5703b834d39c2353e3ec6f
KrishnasaiMandadapu <114215299+KrishnasaiMandadapu@users.noreply.github.com>
2023-11-20 12:48:24 +0530

    Add files via upload

---
+++ Test2/config/end_point_config.py
@@ -0,0 +1,22 @@
+from config.imports import *
+openai.api_key="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"
+# quest_data="/home/support/Semantic_search_3/Embeddings/quest.csv"
+
+os.environ['_BARD_API_KEY'] = 'YAhoQ-pD09RoMqNeQxvntBfen4ybLmVDwZIP51bIt75C4EncYlEssopSSN0g0C8q3v8_7Q.'
+
+token='YAhoQ-pD09RoMqNeQxvntBfen4ybLmVDwZIP51bIt75C4EncYlEssopSSN0g0C8q3v8_7Q.'
+session = requests.Session()
+session.headers = {
+            "Host": "bard.google.com",
+            "X-Same-Domain": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "Origin": "https://bard.google.com",
+            "Referer": "https://bard.google.com/",
+        }
+session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY"))
+
+root='ProdaptGPT'
+image=root+'/resources/prodapt_logo.png'
+conversation_1=root+'/history_logs/conversation_history.json'
+conversation_2=root+'/history_logs/conversation_history_bard.json'
---
+++ Test2/config/imports.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+import openai
+from bardapi import Bard
+import pandas as pd
+import time
+import json
+from streamlit_chat import message
+import os
+import requests
