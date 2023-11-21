d27388e481232762bd714ca01ef26a5fcfcdc363
KrishnasaiMandadapu <114215299+KrishnasaiMandadapu@users.noreply.github.com>
2023-11-21 11:30:10 +0530

    Update end_point_config.py

--- Test2/config/end_point_config.py
+++ Test2/config/end_point_config.py
@@ -2,7 +2,7 @@ from config.imports import *
 openai.api_key="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"
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-
+print(script_dir,end="???????????????")
 # quest_data="/home/support/Semantic_search_3/Embeddings/quest.csv"
 
 os.environ['_BARD_API_KEY'] = 'YAhoQ-pD09RoMqNeQxvntBfen4ybLmVDwZIP51bIt75C4EncYlEssopSSN0g0C8q3v8_7Q.'
@@ -19,10 +19,10 @@ session.headers = {
         }
 session.cookies.set("__Secure-1PSID", os.getenv("_BARD_API_KEY"))
 
-root='/var/lib/jenkins/workspace/Python build/Test2/'
-#image= os.path.join(script_dir, 'resources', 'prodapt_image.png')
-#conversation_1= os.path.join(script_dir, 'history_logs', 'conversation_history.json')
-#conversation_2=os.path.join(script_dir, 'history_logs', 'conversation_history_bard.json')
-image=root+'resources/prodapt_logo.PNG'
-conversation_1=root+'history_logs/conversation_history.json'
-conversation_2=root+'history_logs/conversation_history_bard.json'
+# root='/var/lib/jenkins/workspace/Python build/Test2/'
+image= os.path.join(script_dir, 'resources', 'prodapt_image.png')
+conversation_1= os.path.join(script_dir, 'history_logs', 'conversation_history.json')
+conversation_2=os.path.join(script_dir, 'history_logs', 'conversation_history_bard.json')
+# image=root+'resources/prodapt_logo.PNG'
+# conversation_1=root+'history_logs/conversation_history.json'
+# conversation_2=root+'history_logs/conversation_history_bard.json'
